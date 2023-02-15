8a937f3ca2b809183db3fcb772e77a9ad9df3417
huyneeee <huynq@its-global.vn>
2023-02-15 09:32:01 +0700

    first commit

---
+++ components/layout/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="h-[40px] bg-black">Header</div>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from "next/head";
+import React, { ReactElement, ReactNode } from 'react';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+interface MyAppProps extends AppProps {
+  Component: NextPageWithLayout;
+}
+
+export default function App({ Component, pageProps }: MyAppProps) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='initial-scale=1.0, width=device-width, shrink-to-fit=no'
+        />
+      </Head>
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
